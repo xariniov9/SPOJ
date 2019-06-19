2156ff9421e2748c05a4f85e4258afb732592b62
Himanshu Tiwari <himanshutiw2794@gmail.com>
2019-06-19 17:30:23 +0530

    Update SUMITR.cpp

--- SUMITR.cpp
+++ SUMITR.cpp
@@ -1,3 +1,3 @@
-#include<ios>
-int a[105][105],i,j,t,n;
-main(){scanf("%d",&t);while(t--){scanf("%d",&n);for(i=1;i<=n;i++)for(j=1;j<=i;j++){scanf("%d",&a[i][j]);a[i][j]=std::max(a[i-1][j-1],a[i-1][j])+a[i][j];}j=0;for(i=1;i<=n;i++)j=std::max(j,a[n][i]);printf("%d\n",j);}}
+#include<iostream>
+using namespace std;int I[102][102]={0},N,i,j,t,x,a;
+int main(){cin>>t;while(t--){cin>>N;a=0;for(i=1;i<=N;i++)for(j=1;j<=i;j++){cin>>x;I[i][j]=x+(max(I[i-1][j],I[i-1][j-1]));a=max(a,I[i][j]);}cout<<a<<endl;}return 0;}
